10ef43d01b914cdeb404dd3c28fde88a4fe5d300
hyeonseol00 <hyeonseol22@naver.com>
2024-07-23 12:12:49 +0900

    ✨ update : DB 적용

--- src/handlers/town/enter.handler.js
+++ src/handlers/town/enter.handler.js
@@ -4,7 +4,7 @@ import {
   findUserByUsername,
   insertCharacter,
   insertUserByUsername,
-} from '../../db/backup/coordinates.db.js';
+} from '../../db/user/user.db.js';
 import { getGameSession } from '../../session/game.session.js';
 import { addUser } from '../../session/user.session.js';
 import { handleError } from '../../utils/error/errorHandler.js';
@@ -19,22 +19,35 @@ const enterTownHandler = async ({ socket, userId, payload }) => {
     const user = addUser(socket, nickname);
     gameSession.addUser(user);
 
+    // DB
+    let userInDB = await findUserByUsername(nickname);
+    if (!userInDB) {
+      await insertUserByUsername(nickname);
+      userInDB = await findUserByUsername(nickname);
+    }
+
+    let character = await findCharacterByUserIdAndClass(userInDB.userId, characterClass);
+    if (!character) {
+      await insertCharacter(userInDB, characterClass);
+      character = await findCharacterByUserIdAndClass(userInDB.userId, characterClass);
+    }
+
     const transformInfo = {
-      posX: 0,
+      posX: character.x,
       posY: 1,
-      posZ: 0,
-      rot: 0,
+      posZ: character.z,
+      rot: character.rot,
     };
     const statInfo = {
-      level: 1,
-      hp: 100,
-      maxHp: 100,
-      mp: 100,
-      maxMp: 100,
-      atk: 10,
-      def: 10,
-      magic: 10,
-      speed: 10,
+      level: character.level,
+      hp: character.hp,
+      maxHp: character.hp,
+      mp: character.mp,
+      maxMp: character.mp,
+      atk: character.attack,
+      def: character.defense,
+      magic: character.magic,
+      speed: character.speed,
     };
     const playerInfo = {
       playerId: user.id,
@@ -46,24 +59,6 @@ const enterTownHandler = async ({ socket, userId, payload }) => {
     const enterTownResponse = createResponse('response', 'S_Enter', {
       player: playerInfo,
     });
-
-    // DB
-    const username = nickname;
-    const jobId = characterClass;
-    let userInDB = await findUserByUsername(username);
-    if (!userInDB) {
-      console.log(`${username} user 생성`);
-      await insertUserByUsername(username);
-      userInDB = await findUserByUsername(username);
-    }
-    console.log(userInDB);
-    let character = await findCharacterByUserIdAndClass(userInDB.userId, jobId);
-    if (!character) {
-      console.log(`${jobId} character 생성`);
-      await insertCharacter(userInDB, jobId);
-      character = await findCharacterByUserIdAndClass(userInDB.userId, jobId);
-    }
-    console.log(character);
 
     console.log('현재 접속 중인 유저: ', gameSession.getAllUserIds());
 
