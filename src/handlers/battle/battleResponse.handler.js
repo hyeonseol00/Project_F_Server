169b7bab639bf34b2804b09adf3dff3d058c08a3
hyeonseol00 <hyeonseol22@naver.com>
2024-07-23 19:30:05 +0900

    ✨ update : 현재 던전 세션의 몬스터 수만큼 버튼 생성

--- src/handlers/battle/battleResponse.handler.js
+++ src/handlers/battle/battleResponse.handler.js
@@ -1,14 +1,17 @@
+import { getDungeonByUserId } from '../../session/dungeon.session.js';
 import { getUserBySocket } from '../../session/user.session.js';
 import { createResponse } from '../../utils/response/createResponse.js';
 
 const battleResponseHandler = async ({ socket, payload }) => {
   const user = getUserBySocket(socket);
+  const dungeon = getDungeonByUserId(user.playerId);
 
   switch (payload.responseCode) {
     case 0:
       const btns = [];
-      for (let i = 0; i < 3; i++) {
-        btns.push({ msg: `${i}`, enable: true });
+      for (let i = 0; i < dungeon.monsters.length; i++) {
+        const monster = dungeon.monsters[i];
+        btns.push({ msg: `${monster.name}`, enable: true });
       }
       const battleLog = {
         msg: '공격할 몬스터를 선택하세요.',
@@ -20,15 +23,10 @@ const battleResponseHandler = async ({ socket, payload }) => {
       socket.emit(response);
       break;
     case 1:
-      break;
     case 2:
-      break;
     case 3:
-      break;
     case 4:
-      break;
     case 5:
-      break;
     case 6:
       break;
   }
