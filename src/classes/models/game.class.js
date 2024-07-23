ed75dc45760997555897d71a5e01112a0fbc30a0
hyeonseol00 <hyeonseol22@naver.com>
2024-07-23 19:04:28 +0900

    🔥 del : 미사용 코드 삭제

--- src/classes/models/game.class.js
+++ src/classes/models/game.class.js
@@ -7,15 +7,17 @@ class Game {
     this.id = id;
     this.users = [];
     this.intervalManager = new IntervalManager();
-    this.state = 'waiting'; // 'waiting', 'inProgress'
   }
 
   addUser(user) {
-    if (this.users.length >= MAX_PLAYERS) throw new Error('게임 세션에 자리가 없습니다!');
+    if (this.users.length >= MAX_PLAYERS) {
+      throw new Error('게임 세션에 자리가 없습니다!');
+    }
     this.users.push(user);
 
-    // this.intervalManager.addPlayer(user.id, user.ping.bind(user), 1000);
-    if (this.users.length === MAX_PLAYERS) setTimeout(() => this.startGame(), 3000);
+    if (this.users.length === MAX_PLAYERS) {
+      setTimeout(() => this.startGame(), 3000);
+    }
   }
 
   getUser(userId) {
@@ -31,38 +33,6 @@ class Game {
   removeUser(userId) {
     this.users = this.users.filter((user) => user.id !== userId);
     this.intervalManager.removePlayer(userId);
-
-    if (this.users.length < MAX_PLAYERS) this.state = 'waiting';
-  }
-
-  startGame() {
-    this.state = 'inProgress';
-  }
-
-  getMaxLatency() {
-    let maxLatency = 0;
-    this.users.forEach((user) => {
-      maxLatency = Math.max(maxLatency, user.latency);
-    });
-
-    return maxLatency;
-  }
-
-  getAllLocation(userId, newX, newY) {
-    const maxLatency = this.getMaxLatency();
-
-    const locationData = this.users.map((user) => {
-      let [argX, argY] = [newX, newY];
-      if (user.id != userId) [argX, argY] = [0, 0];
-
-      const { x, y } = user.calculatePosition(maxLatency, argX, argY);
-
-      if (user.id == userId) user.updatePosition(x, y);
-
-      return { id: user.id, playerId: user.playerId, x, y };
-    });
-
-    return createLocationPacket(locationData);
   }
 }
 
