5e2bb4339ca8b392b17624f27b1d6384437fd910
hyeonseol00 <hyeonseol22@naver.com>
2024-07-23 19:29:21 +0900

    ✨ update : 던전 진입 핸들러에서 던전 세션 생성

--- src/handlers/town/enterDungeon.handler.js
+++ src/handlers/town/enterDungeon.handler.js
@@ -1,33 +1,33 @@
 import { handleError } from '../../utils/error/errorHandler.js';
 import { createResponse } from '../../utils/response/createResponse.js';
 import { getUserBySocket } from '../../session/user.session.js';
-import {
-  findCharacterByUserIdAndNickname,
-  findUserByUsername,
-} from '../../db/user/user.db.js';
+import { findCharacterByUserIdAndNickname, findUserByUsername } from '../../db/user/user.db.js';
+import { addDungeon } from '../../session/dungeon.session.js';
 
-const enterDungeonHandler = async ({ socket, userId, payload }) => {
+const enterDungeonHandler = async ({ socket, payload }) => {
   try {
     const { dungeonCode } = payload;
     const user = getUserBySocket(socket);
-    const nickname =user.playerId;
+    const nickname = user.playerId;
+    const dungeon = addDungeon(user.playerId);
 
-    let userInDB = await findUserByUsername(nickname);
-    let character = await findCharacterByUserIdAndNickname(userInDB.userId, userInDB.username);
-    const monsterStatus = [
-      {
-        monsterIdx :0,
-        monsterModel: 2001,
-        monsterName: '몬스터',
-        monsterHp: 100.0,
-      },
-      {
-        monsterIdx: 1,
-        monsterModel: 2004,
-        monsterName: '몬스터2',
-        monsterHp: 300.0,
-      },
-    ];
+    // 이 부분 지금 하드코딩임. 데이터베이스에서 받아와서 랜덤으로 뽑는 등 처리 요망
+    dungeon.addMonster(0, 2001, 300, 60, '빨간버섯');
+    dungeon.addMonster(1, 2002, 300, 60, '외눈슬라임');
+
+    const userInDB = await findUserByUsername(nickname);
+    const character = await findCharacterByUserIdAndNickname(userInDB.userId, userInDB.username);
+
+    const monsterStatus = [];
+    for (let i = 0; i < dungeon.monsters.length; i++) {
+      const monster = dungeon.monsters[i];
+      monsterStatus.push({
+        monsterIdx: monster.idx,
+        monsterModel: monster.id,
+        monsterName: monster.name,
+        monsterHp: monster.hp,
+      });
+    }
 
     const dungeonInfo = {
       dungeonCode: dungeonCode + 5000,
@@ -62,7 +62,7 @@ const enterDungeonHandler = async ({ socket, userId, payload }) => {
     };
 
     const ScreenText = {
-      msg: 'aaa',
+      msg: 'screen_text_test',
       typingAnimation: true,
       alignment: ScreenTextAlignment,
       textColor: textColor,
@@ -70,12 +70,12 @@ const enterDungeonHandler = async ({ socket, userId, payload }) => {
     };
 
     const BtnInfo = {
-      msg: 'aa',
+      msg: 'btn_test',
       enable: true,
     };
 
     const BattleLog = {
-      msg: 'aa',
+      msg: 'battle_log_test',
       typingAnimation: true,
       btns: BtnInfo,
     };
