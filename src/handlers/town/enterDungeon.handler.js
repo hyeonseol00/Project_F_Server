e34652e02d647bdbde541571c2231d022c2652fd
KR-EGOIST <jhso5041@naver.com>
2024-07-23 15:06:07 +0900

    🐛 bugfix : 던전 입장 버그 해결
    resolve #15

--- src/handlers/town/enterDungeon.handler.js
+++ src/handlers/town/enterDungeon.handler.js
@@ -1,74 +1,81 @@
+import { handleError } from '../../utils/error/errorHandler.js';
+import { createResponse } from '../../utils/response/createResponse.js';
 
 const enterDungeonHandler = async ({ socket, userId, payload }) => {
-    try {
-      const { dungeonCode } = payload;
+  try {
+    const { dungeonCode } = payload;
 
-      const monsterStatus ={
-        monsterIdx,
-        monsterModel,
-        monsterName,
-        monsterHp,
-      }
+    const monsterStatus = [
+      {
+        monsterIdx: 2001,
+        monsterModel: 0,
+        monsterName: '몬스터',
+        monsterHp: 100.0,
+      },
+    ];
 
-      const dungeonInfo = {
-        dungeonCode,
-        monsters: monsterStatus,
-      }
+    const dungeonInfo = {
+      dungeonCode: dungeonCode + 5000,
+      monsters: monsterStatus,
+    };
 
-      const playerStatus = {
-        playerClass,
-        playerLevel,
-        playerName,
-        playerFullHp,
-        playerFullMp,
-        playerCurHp,
-        playerCurMp,
-      }
+    const playerStatus = {
+      playerClass: 1,
+      playerLevel: 1,
+      playerName: 'aa',
+      playerFullHp: 100.0,
+      playerFullMp: 100.0,
+      playerCurHp: 100.0,
+      playerCurMp: 100.0,
+    };
 
-      const ScreenTextAlignment = {
-        x,
-        y,
-      }
+    const ScreenTextAlignment = {
+      x: 0,
+      y: 0,
+    };
 
-      const Color = {
-        r,
-        g,
-        b,
-      }
+    const textColor = {
+      r: 255,
+      g: 255,
+      b: 255,
+    };
 
-      const ScreenText = {
-        msg,
-        typingAnimation,
-        alignment: ScreenTextAlignment,
-        textColor : Color,
-        screenColor: Color,
-      }
+    const screenColor = {
+      r: 0,
+      g: 0,
+      b: 0,
+    };
 
-      const BtnInfo = {
-        msg,
-        enable,
-      }
+    const ScreenText = {
+      msg: 'aaa',
+      typingAnimation: true,
+      alignment: ScreenTextAlignment,
+      textColor: textColor,
+      screenColor: screenColor,
+    };
 
-      const BattleLog ={
-        msg,
-        typingAnimation,
-        btns: BtnInfo, 
-      }
+    const BtnInfo = {
+      msg: 'aa',
+      enable: true,
+    };
 
-      const enterDungeonResponse = createResponse('response', 'S_EnterDungeon', {
-        dungeonInfo : dungeonInfo,
-        player : playerStatus,
-        ScreenText : ScreenText,
-        battleLog : BattleLog,
-      });
-      
+    const BattleLog = {
+      msg: 'aa',
+      typingAnimation: true,
+      btns: BtnInfo,
+    };
 
+    const enterDungeonResponse = createResponse('response', 'S_EnterDungeon', {
+      dungeonInfo: dungeonInfo,
+      player: playerStatus,
+      ScreenText: ScreenText,
+      battleLog: BattleLog,
+    });
 
-      socket.write(enterDungeonResponse);
-    } catch (err) {
-      handleError(socket, err);
-    }
-  };
-  
-  export default enterDungeonHandler;
-  
+    socket.write(enterDungeonResponse);
+  } catch (err) {
+    handleError(socket, err);
+  }
+};
+
+export default enterDungeonHandler;
