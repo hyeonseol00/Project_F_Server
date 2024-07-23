d5f65539b5668508c0d67c5f88f82ec23023547a
hyeonseol00 <hyeonseol22@naver.com>
2024-07-23 19:29:45 +0900

    🎉 add : 던전 세션에 몬스터 정보 추가

--- src/classes/models/dungeon.class.js
+++ src/classes/models/dungeon.class.js
@@ -1,9 +1,21 @@
 import { config } from '../../config/config.js';
+import Monster from './monster.class.js';
 
 class InstanceDungeon {
   constructor(userId) {
     this.id = userId;
+    this.monsters = [];
     this.battleSceneStatus = config.sceneStatus.message;
+  }
+
+  addMonster(idx, id, hp, power, name) {
+    const monster = new Monster(idx, id, hp, power, name);
+    this.monsters.push(monster);
+  }
+
+  removeMonster(idx) {
+    const target = this.monsters.findIndex((monster) => monster.idx == idx);
+    this.monsters.slice(target, 1);
   }
 }
 
---
+++ src/classes/models/monster.class.js
@@ -0,0 +1,11 @@
+class Monster {
+  constructor(idx, id, hp, power, name) {
+    this.idx = idx;
+    this.id = id;
+    this.hp = hp;
+    this.power = power;
+    this.name = name;
+  }
+}
+
+export default Monster;
