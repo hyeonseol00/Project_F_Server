71185a846856ef64b2cc8872293a1cd062484132
hyeonseol00 <hyeonseol22@naver.com>
2024-07-24 20:38:56 +0900

    ✨ update : 플레이어가 공격 시 몬스터 체력 실제로 깎이게 업데이트

--- src/handlers/battle/battleFlows/target.battle.js
+++ src/handlers/battle/battleFlows/target.battle.js
@@ -4,16 +4,17 @@ import { createResponse } from '../../../utils/response/createResponse.js';
 export default function targetMonsterScene(responseCode, dungeon, socket) {
   const btns = [{ msg: '다음', enable: true }];
   const targetMonsterIdx = responseCode - 1;
+  const player = dungeon.player;
+  const monster = dungeon.monsters[targetMonsterIdx];
 
   const battleLog = {
-    msg: `몬스터 ${dungeon.monsters[targetMonsterIdx].name}을(를) 공격합니다!`,
+    msg: `몬스터 ${monster.name}을(를) 공격합니다!`,
     typingAnimation: false,
     btns,
   };
   const responseBattleLog = createResponse('response', 'S_BattleLog', { battleLog });
   socket.write(responseBattleLog);
 
-  // 플레이어 공격 모션은 안 나옴..
   const actionSet = {
     animCode: 0,
     effectCode: 3001,
@@ -23,6 +24,13 @@ export default function targetMonsterScene(responseCode, dungeon, socket) {
     actionSet,
   });
   socket.write(responsePlayerAction);
+
+  monster.hp -= player.attack;
+  const responseSetMonsterHp = createResponse('response', 'S_SetMonsterHp', {
+    monsterIdx: targetMonsterIdx,
+    hp: monster.hp,
+  });
+  socket.write(responseSetMonsterHp);
 
   dungeon.battleSceneStatus = config.sceneStatus.playerAtk;
 }
