31752f3fd6bb8bd0f8cd3088a71c5874f1e08ea0
KR-EGOIST <jhso5041@naver.com>
2024-07-24 12:08:08 +0900

    ♻️ refactor : town_game DB 연동

--- src/config/config.js
+++ src/config/config.js
@@ -8,6 +8,11 @@ import {
   DB1_PASSWORD,
   DB1_HOST,
   DB1_PORT,
+  DB2_NAME,
+  DB2_USER,
+  DB2_PASSWORD,
+  DB2_HOST,
+  DB2_PORT,
 } from '../constants/env.js';
 import { PACKET_TYPE_LENGTH, TOTAL_LENGTH } from '../constants/header.js';
 import { TOWN_SESSION_ID } from '../constants/session.js';
@@ -34,6 +39,13 @@ export const config = {
       password: DB1_PASSWORD,
       host: DB1_HOST,
       port: DB1_PORT,
+    },
+    TOWN_GAME: {
+      name: DB2_NAME,
+      user: DB2_USER,
+      password: DB2_PASSWORD,
+      host: DB2_HOST,
+      port: DB2_PORT,
     },
   },
   sceneStatus: {
