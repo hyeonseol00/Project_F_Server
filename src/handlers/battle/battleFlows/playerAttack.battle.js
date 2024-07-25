c2a5e5ad166af4e17a3280617d9ee7584563205b
KR-EGOIST <jhso5041@naver.com>
2024-07-25 20:32:30 +0900

    🐛 bugfix : 플레이어 공격 후 몬스터가 순서대로 공격한 뒤 다시 처음 배틀씬으로 이동

--- src/handlers/battle/battleFlows/playerAttack.battle.js
+++ src/handlers/battle/battleFlows/playerAttack.battle.js
@@ -1,13 +1,13 @@
 import { config } from '../../../config/config.js';
 import { createResponse } from '../../../utils/response/createResponse.js';
+import switchToActionScene from './switchScene/action.switch.js';
 
-export default function playerAttackScene(responseCode, dungeon, socket) {
+export default function playerAttackScene(responseCode, dungeon, socket, index) {
   if (responseCode == 1) {
     const btns = [{ msg: '다음', enable: true }];
-    const targetMonsterIdx = 0;
 
     const battleLog = {
-      msg: `몬스터 ${dungeon.monsters[targetMonsterIdx].name}이(가) 플레이어를 공격합니다!`,
+      msg: `몬스터 ${dungeon.monsters[index].name}이(가) 플레이어를 공격합니다!`,
       typingAnimation: false,
       btns,
     };
@@ -19,11 +19,15 @@ export default function playerAttackScene(responseCode, dungeon, socket) {
       effectCode: 3002,
     };
     const monsterAction = createResponse('response', 'S_MonsterAction', {
-      actionMonsterIdx: targetMonsterIdx,
+      actionMonsterIdx: index,
       actionSet,
     });
     socket.write(monsterAction);
 
-    dungeon.battleSceneStatus = config.sceneStatus.enemyAtk;
+    if (index === 2) {
+      dungeon.battleSceneStatus = switchToActionScene(dungeon, socket);
+    } else {
+      dungeon.battleSceneStatus = config.sceneStatus.enemyAtk;
+    }
   }
 }
--- src/handlers/battle/battleResponse.handler.js
+++ src/handlers/battle/battleResponse.handler.js
@@ -8,6 +8,8 @@ import targetMonsterScene from './battleFlows/target.battle.js';
 import playerAttackScene from './battleFlows/playerAttack.battle.js';
 import chooseSkillType from './battleFlows/skill.battle.js';
 
+let index = 0;
+
 const battleResponseHandler = async ({ socket, payload }) => {
   const user = getUserBySocket(socket);
   const dungeon = getDungeonByUserId(user.nickname);
@@ -28,9 +30,14 @@ const battleResponseHandler = async ({ socket, payload }) => {
       targetMonsterScene(responseCode, dungeon, socket, true);
       break;
     case config.sceneStatus.playerAtk:
-      await playerAttackScene(responseCode, dungeon, socket);
+      console.log('playerAtk : ', index);
+      await playerAttackScene(responseCode, dungeon, socket, index);
+      index++;
       break;
     case config.sceneStatus.enemyAtk:
+      console.log('enemyAtk : ', index);
+      await playerAttackScene(responseCode, dungeon, socket, index);
+      index++;
       break;
     case config.sceneStatus.confirm:
       await confirmScene(responseCode, dungeon, user.nickname, socket);
@@ -40,6 +47,7 @@ const battleResponseHandler = async ({ socket, payload }) => {
       break;
   }
   console.log('battleSceneStatus: ', dungeon.battleSceneStatus);
+  index = index % 3;
 };
 
 export default battleResponseHandler;
