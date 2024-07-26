15f080ef5f9a29bda5ec679b7a5c95242c3b0eee
hyeonseol00 <hyeonseol22@naver.com>
2024-07-26 20:11:48 +0900

    🐛 bugfix : 누락 코드 보충

--- src/handlers/battle/battleFlows/gameOverLose.battle.js
+++ src/handlers/battle/battleFlows/gameOverLose.battle.js
@@ -2,6 +2,8 @@ import { removeDungeon } from '../../../session/dungeon.session.js';
 import { createResponse } from '../../../utils/response/createResponse.js';
 
 export default function gameOverLoseScene(responseCode, dungeon, socket) {
+  const nickname = dungeon.player.nickname;
+
   if (responseCode == 1) {
     const responseLeaveDungeon = createResponse('response', 'S_LeaveDungeon', {});
     socket.write(responseLeaveDungeon);
