8a4e5412eca2d6ab510843012d10eb19178dd13f
pledge24 <104922729+pledge24@users.noreply.github.com>
2024-07-29 17:22:38 +0900

    🎉 add. chatCommands.js, team.handler.js 빈 프레임 추가

--- src/handlers/chatCommands.js
+++ src/handlers/chatCommands.js
@@ -1,17 +1,25 @@
-import { sendDirectMessage } from "./town/chatCommand.handler.js";
+import { sendDirectMessage } from './town/chatCommand.handler.js';
+import {
+  sendMessageToTeam,
+  createTeamHandler,
+  joinTeamHandler,
+  leaveTeamHandler,
+  inviteTeamHandler,
+  acceptTeamHandler,
+  kickMemberHandler,
+} from './town/team.handler.js';
 
 const chatCommands = new Map([
-  [
-    'w', sendDirectMessage
-  ],
-  // [
-  //   'team',
-  //   function (rawMsg, teamId) {
-  //     const msg = `[팀] ${rawMsg}`;
-  //     return msg;
-  //   },
-  // ],
-  
+  ['w', sendDirectMessage],
+  // ---------team begin------------
+  ['t', sendMessageToTeam],
+  ['createTeam', createTeamHandler],
+  ['joinTeam', joinTeamHandler],
+  ['leaveTeam', leaveTeamHandler],
+  ['inviteTeam', inviteTeamHandler],
+  ['acceptTeam', acceptTeamHandler],
+  ['kickMember', kickMemberHandler],
+  // ---------team end------------
 ]);
 
 export default chatCommands;
---
+++ src/handlers/town/team.handler.js
@@ -0,0 +1,27 @@
+export const sendMessageToTeam = () => {
+
+}
+
+export const createTeamHandler = () => {
+    
+}
+
+export const joinTeamHandler = () => {
+    
+}
+
+export const leaveTeamHandler = () => {
+    
+}
+
+export const inviteTeamHandler = () => {
+    
+}
+
+export const acceptTeamHandler = () => {
+    
+}
+
+export const kickMemberHandler = () => {
+    
+}
