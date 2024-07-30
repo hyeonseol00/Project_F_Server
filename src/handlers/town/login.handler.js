02657dea5edc5d4f3973f51edcaab201f96ff87a
KR-EGOIST <jhso5041@naver.com>
2024-07-30 18:05:24 +0900

    ✨ update : response 에 message 추가

--- src/handlers/town/login.handler.js
+++ src/handlers/town/login.handler.js
@@ -1,4 +1,6 @@
 import { handleError } from '../../utils/error/errorHandler.js';
+import { createResponse } from '../../utils/response/createResponse.js';
+import { findUserByUsername } from '../../db/user/user.db.js';
 
 const loginHandler = async ({ socket, payload }) => {
   try {
@@ -16,10 +18,12 @@ const loginHandler = async ({ socket, payload }) => {
     if (flag) {
       response = createResponse('response', 'S_LogIn', {
         success: true,
+        message: '로그인을 성공했습니다.',
       });
     } else {
       response = createResponse('response', 'S_LogIn', {
         success: false,
+        message: '아이디 또는 비밀번호가 틀렸습니다.',
       });
     }
 
--- src/handlers/town/register.handler.js
+++ src/handlers/town/register.handler.js
@@ -6,6 +6,7 @@ const registerHandler = async ({ socket, payload }) => {
   try {
     const { nickname, password } = payload;
     let response;
+    let message;
     let flag = true;
 
     // DB에서 nickname 중복 체크
@@ -18,18 +19,35 @@ const registerHandler = async ({ socket, payload }) => {
       flag = false;
     }
 
+    if (nickname.trim().length === 0) {
+      flag = false;
+      message = '아이디를 입력하세요.';
+    } else if (nickname.length < 2) {
+      flag = false;
+      message = '아이디는 2자 이상으로 입력하세요.';
+    } else if (nickname.length > 10) {
+      flag = false;
+      message = '아이디는 10자 이하로 입력하세요.';
+    }
+
     if (password.trim().length === 0) {
       flag = false;
+      message = '비밀번호를 입력하세요.';
+    } else if (password.length < 6) {
+      flag = false;
+      message = '비밀번호는 6자 이상으로 입력하세요.';
     }
 
     // 성공시 response 전달
     if (flag) {
       response = createResponse('response', 'S_Register', {
         success: true,
+        message: '회원가입을 성공했습니다.',
       });
     } else {
       response = createResponse('response', 'S_Register', {
         success: false,
+        message: message,
       });
     }
 
