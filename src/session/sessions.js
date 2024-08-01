76bac9e6506e5a2d4580e50bc6af6ff908192561
hyeonseol00 <hyeonseol22@naver.com>
2024-08-01 21:14:02 +0900

    🎉 add : 버그의 둥지 세션 추가

--- src/session/sessions.js
+++ src/session/sessions.js
@@ -1,5 +1,8 @@
+import Hatchery from '../classes/models/hatchery.class.js';
+
 export const userSessions = [];
 export const gameSessions = [];
 export const instanceDungeonSessions = [];
 export const GAAPP = { userCount: 0 };
 export const levelTable = [];
+export const hatcherySession = new Hatchery();
