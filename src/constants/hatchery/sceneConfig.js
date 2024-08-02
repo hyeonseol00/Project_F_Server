24cb35be29493455dbde98a903f16c53f82cdeae
hyeonseol00 <hyeonseol22@naver.com>
2024-08-02 16:40:20 +0900

    ✨ update : 둥지 입장 시 다른 플레이어들에게 정보 전달

--- src/constants/hatchery/sceneConfig.js
+++ src/constants/hatchery/sceneConfig.js
@@ -1,1 +1,2 @@
+export const MAX_PLAYERS = 4;
 export const BOSS_ID = 2028;
--- src/handlers/town/enterHatchery.handler.js
+++ src/handlers/town/enterHatchery.handler.js
@@ -2,6 +2,7 @@ import { config } from '../../config/config.js';
 import { getGameSession } from '../../session/game.session.js';
 import { getHatcherySession } from '../../session/hatchery.session.js';
 import { getUserBySocket } from '../../session/user.session.js';
+import { handleError } from '../../utils/error/errorHandler.js';
 import { createResponse } from '../../utils/response/createResponse.js';
 
 const enterHatcheryHandler = async ({ socket, payload }) => {
@@ -12,6 +13,10 @@ const enterHatcheryHandler = async ({ socket, payload }) => {
 
     const { hp, maxHp, name } = hatcherySession.monster;
 
+    gameSession.removeUser(user.playerId);
+    hatcherySession.addPlayer(user);
+
+    /***** S_EnterHatchery *****/
     const enterHatcheryResponse = createResponse('response', 'S_EnterHatchery', {
       player: user.getPlayerInfo(),
       bossMaxHp: maxHp,
@@ -20,11 +25,30 @@ const enterHatcheryHandler = async ({ socket, payload }) => {
 
     socket.write(enterHatcheryResponse);
 
+    /***** S_SetHatcheryBossHp *****/
     const setHatcheryBossHpResponse = createResponse('response', 'S_SetHatcheryBossHp', {
       bossCurHp: hp,
     });
 
     socket.write(setHatcheryBossHpResponse);
+
+    /***** S_SpawnPlayerHatchery *****/
+    const playerInfos = [];
+    for (const player of hatcherySession.players) {
+      playerInfos.push(player.playerInfo);
+    }
+
+    for (const player of hatcherySession.players) {
+      const filterdPlayerInfos = playerInfos.filter(
+        (playerInfo) => playerInfo.playerId !== player.playerId,
+      );
+
+      const spawnHatcheryResponse = createResponse('response', 'S_SpawnPlayerHatchery', {
+        players: filterdPlayerInfos,
+      });
+
+      player.socket.write(spawnHatcheryResponse);
+    }
   } catch (err) {
     handleError(socket, err);
   }
