e978d82944d763578d09fc3f67732395b2b7ad0e
hyeonseol00 <hyeonseol22@naver.com>
2024-08-05 21:08:39 +0900

    ♻️ refactor : 가독성, 중복 코드 리팩터
    Co-authored-by: 김동헌 <znfnfns0365@users.noreply.github.com>

--- src/handlers/town/enterHatchery.handler.js
+++ src/handlers/town/enterHatchery.handler.js
@@ -11,22 +11,26 @@ const enterHatcheryHandler = async ({ socket, payload }) => {
     const hatcherySession = getHatcherySession();
     const user = getUserBySocket(socket);
 
-    const { hp, maxHp, name, transform } = hatcherySession.boss;
+    const { hp, maxHp, name, transform, speed } = hatcherySession.boss;
+    const { posX, posY, posZ, rot } = transform;
 
     gameSession.removeUser(user.playerId);
     hatcherySession.addPlayer(user);
 
     /***** S_EnterHatchery *****/
-    const bossTransformInfo = {
-      posX: transform.x,
-      poxY: transform.y,
-      posZ: transform.z,
-      rot: transform.rot,
+    const transformInfo = {
+      posX: Math.random() * 18 - 9, // -9 ~ 9
+      posY: 1.0,
+      posZ: Math.random() * 16 - 8, // -8 ~ 8
+      rot: Math.random() * 360, // 0 ~ 360
     };
+    user.setTransformInfo(transformInfo);
+    const bossTransformInfo = { posX, posY, posZ, rot };
     const enterHatcheryResponse = createResponse('response', 'S_EnterHatchery', {
       player: user.getPlayerInfo(),
       bossTransformInfo,
       bossMaxHp: maxHp,
+      bossSpeed: speed,
       bossName: name,
     });
 
