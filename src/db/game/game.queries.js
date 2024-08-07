8cb6718bf4caf2027268bcb43ed5e0a4fca7c66c
znfnfns0365 <znfnfns0365@naver.com>
2024-08-07 20:01:53 +0900

    ♻️ refactor : DB 테이블명 snake_case로 수정 (migration, queries 적용)

--- src/db/game/game.queries.js
+++ src/db/game/game.queries.js
@@ -1,10 +1,10 @@
 export const SQL_GAME_QUERIES = {
-  FIND_MONSTERS_BY_DUNGEON_ID: 'SELECT * FROM Dungeon_Monsters WHERE dungeon_id = ?',
-  FIND_MONSTER_BY_ID: 'SELECT * FROM Monsters WHERE monster_id = ?',
-  GET_MONSTER_EFFECT_BY_ID: 'SELECT monster_effect FROM Monsters WHERE monster_id = ?',
-  GET_ALL_LEVEL_DATA: 'SELECT * FROM Levels',
-  FIND_ITEMS_BY_DUNGEON_CODE: 'SELECT * FROM Dungeon_Items WHERE dungeon_id = ?',
-  GET_ITEM: 'SELECT * FROM Items WHERE item_id = ?',
-  GET_ALL_ITEM_DATA: 'SELECT * FROM Items',
-  GET_MONSTER: 'SELECT * FROM Monsters',
+  FIND_MONSTERS_BY_DUNGEON_ID: 'SELECT * FROM dungeon_monsters WHERE dungeon_id = ?',
+  FIND_MONSTER_BY_ID: 'SELECT * FROM monsters WHERE monster_id = ?',
+  GET_MONSTER_EFFECT_BY_ID: 'SELECT monster_effect FROM monsters WHERE monster_id = ?',
+  GET_ALL_LEVEL_DATA: 'SELECT * FROM levels',
+  FIND_ITEMS_BY_DUNGEON_CODE: 'SELECT * FROM dungeon_items WHERE dungeon_id = ?',
+  GET_ITEM: 'SELECT * FROM items WHERE item_id = ?',
+  GET_ALL_ITEM_DATA: 'SELECT * FROM items',
+  GET_MONSTER: 'SELECT * FROM monsters',
 };
