95e03b603adde3d11dea14bb232f37809558f246
hyeonseol00 <hyeonseol22@naver.com>
2024-08-07 20:56:39 +0900

    ♻️ refactor : 객체 인자 예외처리

--- src/classes/models/item.class.js
+++ src/classes/models/item.class.js
@@ -1,25 +1,32 @@
 class Item {
   constructor(quantity, itemInfo) {
-    this.itemId = itemInfo.itemId;
-    this.itemType = itemInfo.itemType;
-    this.name = itemInfo.itemName;
-    this.quantity = quantity;
-    this.requireLevel = itemInfo.requireLevel;
-    if (this.itemType === 'potion') {
-      // potions
-      this.hp = itemInfo.itemHp;
-      this.mp = itemInfo.itemMp;
-    } else {
-      // mounting items
-      this.addHp = itemInfo.itemHp;
-      this.addMp = itemInfo.itemMp;
-      this.addAttack = itemInfo.itemAttack;
-      this.addDefense = itemInfo.itemDefense;
-      this.addMagic = itemInfo.itemMagic;
-      this.addSpeed = itemInfo.itemSpeed;
-      this.cost = itemInfo.itemCost;
-      this.addAvoidance = itemInfo.itemAvoidance;
-      this.addCritical = itemInfo.itemCritical;
+    try {
+      if (!itemInfo) {
+        return;
+      }
+      this.itemId = itemInfo.itemId;
+      this.itemType = itemInfo.itemType;
+      this.name = itemInfo.itemName;
+      this.quantity = quantity;
+      this.requireLevel = itemInfo.requireLevel;
+      if (this.itemType === 'potion') {
+        // potions
+        this.hp = itemInfo.itemHp;
+        this.mp = itemInfo.itemMp;
+      } else {
+        // mounting items
+        this.addHp = itemInfo.itemHp;
+        this.addMp = itemInfo.itemMp;
+        this.addAttack = itemInfo.itemAttack;
+        this.addDefense = itemInfo.itemDefense;
+        this.addMagic = itemInfo.itemMagic;
+        this.addSpeed = itemInfo.itemSpeed;
+        this.cost = itemInfo.itemCost;
+        this.addAvoidance = itemInfo.itemAvoidance;
+        this.addCritical = itemInfo.itemCritical;
+      }
+    } catch (err) {
+      throw new Error(err);
     }
   }
 }
