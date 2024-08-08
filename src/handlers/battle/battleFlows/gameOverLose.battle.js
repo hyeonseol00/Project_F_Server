db88df98587c0097218dae1942279c2c6bd2c8a7
hyeonseol00 <hyeonseol22@naver.com>
2024-08-08 16:56:07 +0900

    🩹 fix : 전역변수 위치 수정

--- src/handlers/battle/battleFlows/gameOverLose.battle.js
+++ src/handlers/battle/battleFlows/gameOverLose.battle.js
@@ -1,13 +1,13 @@
+import { config } from '../../../config/config.js';
 import { removeDungeon } from '../../../session/dungeon.session.js';
 import { createResponse } from '../../../utils/response/createResponse.js';
 
-const respawnHp = 50;
 export default function gameOverLoseScene(responseCode, dungeon, socket) {
   const nickname = dungeon.player.nickname;
 
   const user = dungeon.player; // user 클래스
   const playerStatus = user.playerInfo.statInfo;
-  playerStatus.hp = respawnHp; // 플레이어 사망 시 체력 50으로 마을로 귀한
+  playerStatus.hp = config.battleScene.respawnHp; // 플레이어 사망 시 체력 50으로 마을로 귀한
 
   if (responseCode == 1) {
     const responseLeaveDungeon = createResponse('response', 'S_LeaveDungeon', {});
