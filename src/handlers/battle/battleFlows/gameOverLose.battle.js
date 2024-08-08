845a0234ace829aefac6078c92ef7ddf3f54498c
KR-EGOIST <jhso5041@naver.com>
2024-08-08 15:06:01 +0900

    🐛 bugfix : 플레이어 사망 시 체력 50인 상태로 마을로 귀환
    resolve #173

--- src/handlers/battle/battleFlows/gameOverLose.battle.js
+++ src/handlers/battle/battleFlows/gameOverLose.battle.js
@@ -4,6 +4,10 @@ import { createResponse } from '../../../utils/response/createResponse.js';
 export default function gameOverLoseScene(responseCode, dungeon, socket) {
   const nickname = dungeon.player.nickname;
 
+  const user = dungeon.player; // user 클래스
+  const playerStatus = user.playerInfo.statInfo;
+  playerStatus.hp = 50; // 플레이어 사망 시 체력 50으로 마을로 귀한
+
   if (responseCode == 1) {
     const responseLeaveDungeon = createResponse('response', 'S_LeaveDungeon', {});
     socket.write(responseLeaveDungeon);
