6d35f3d42b66667ed3353181ce465e88f5436294
KR-EGOIST <jhso5041@naver.com>
2024-08-08 16:48:17 +0900

    🩹 fix : 매직 넘버 수정

--- src/handlers/battle/battleFlows/gameOverLose.battle.js
+++ src/handlers/battle/battleFlows/gameOverLose.battle.js
@@ -1,12 +1,13 @@
 import { removeDungeon } from '../../../session/dungeon.session.js';
 import { createResponse } from '../../../utils/response/createResponse.js';
 
+const respawnHp = 50;
 export default function gameOverLoseScene(responseCode, dungeon, socket) {
   const nickname = dungeon.player.nickname;
 
   const user = dungeon.player; // user 클래스
   const playerStatus = user.playerInfo.statInfo;
-  playerStatus.hp = 50; // 플레이어 사망 시 체력 50으로 마을로 귀한
+  playerStatus.hp = respawnHp; // 플레이어 사망 시 체력 50으로 마을로 귀한
 
   if (responseCode == 1) {
     const responseLeaveDungeon = createResponse('response', 'S_LeaveDungeon', {});
