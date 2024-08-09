37f6daf32254801be4cb763d27e80815cd29878b
hyeonseol00 <hyeonseol22@naver.com>
2024-08-09 10:14:27 +0900

    🐛 bugfix : onEnd, onError try/catch 처리

--- src/events/onError.js
+++ src/events/onError.js
@@ -7,25 +7,29 @@ import CustomError from '../utils/error/customError.js';
 import { handleError } from '../utils/error/errorHandler.js';
 
 export const onError = (socket) => (err) => {
-  handleError(socket, new CustomError(500, `소켓 오류: ${err.message}`));
+  try {
+    handleError(socket, new CustomError(500, `소켓 오류: ${err.message}`));
 
-  const user = getUserBySocket(socket);
-  const gameSession = getGameSession(config.session.townId);
-  const hatcherySession = getHatcherySession();
+    const user = getUserBySocket(socket);
+    const gameSession = getGameSession(config.session.townId);
+    const hatcherySession = getHatcherySession();
 
-  if (user) {
-    const sessionItems = [...user.potions, ...user.mountingItems];
+    if (user) {
+      const sessionItems = [...user.potions, ...user.mountingItems];
 
-    gameSession.removeUser(user.playerId);
-    hatcherySession.removePlayer(user.nickname);
+      gameSession.removeUser(user.playerId);
+      hatcherySession.removePlayer(user.nickname);
 
-    updateCharacterStatus(user);
-    updateCharacterItems(user.characterId, sessionItems);
+      updateCharacterStatus(user);
+      updateCharacterItems(user.characterId, sessionItems);
+    }
+    removeDungeon(user.nickname);
+
+    console.log('클라이언트 연결이 해제되었습니다: ', socket.remoteAddress, socket.remotePort);
+    console.log('현재 접속 중인 유저: ', gameSession.getAllUserIds());
+
+    removeUser(socket);
+  } catch (err) {
+    handleError(socket, err);
   }
-  removeDungeon(user.nickname);
-
-  console.log('클라이언트 연결이 해제되었습니다: ', socket.remoteAddress, socket.remotePort);
-  console.log('현재 접속 중인 유저: ', gameSession.getAllUserIds());
-
-  removeUser(socket);
 };
