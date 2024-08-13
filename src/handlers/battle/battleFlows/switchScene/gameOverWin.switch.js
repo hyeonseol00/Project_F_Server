2e62c135bdeaf707a83be0b6cb95ee6ebb1c271d
선재영 <wodud1892@naver.com>
2024-08-13 21:06:43 +0900

    🐛 bugfix : 전투 승리 후 전리품 아이템 획득 버그 픽스

--- src/handlers/battle/battleFlows/switchScene/gameOverWin.switch.js
+++ src/handlers/battle/battleFlows/switchScene/gameOverWin.switch.js
@@ -2,6 +2,7 @@ import { getItemById } from '../../../../assets/item.assets.js';
 import { config } from '../../../../config/config.js';
 import { createResponse } from '../../../../utils/response/createResponse.js';
 import { getItemIdx } from '../../../../classes/DBgateway/playerinfo.gateway.js';
+import { addItem, pushItem } from '../../../../classes/DBgateway/playerinfo.gateway.js';
 export default async function switchToGameOverWin(dungeon, socket) {
   const item = dungeon.selectItem;
   const itemInfo = await getItemById(item.itemId);
@@ -11,10 +12,12 @@ export default async function switchToGameOverWin(dungeon, socket) {
   const itemIdx = await getItemIdx(item.itemId);
   if (itemIdx === -1) {
     // 아이템이 없으면 추가
-    player.items.push(item);
+    // player.items.push(item);
+    await pushItem(socket, item);
   } else {
     // 있으면 quantity 증가
-    player.items[itemIdx].quantity += item.quantity;
+    // player.items[itemIdx].quantity += item.quantity;
+    await addItem(socket, item.itemId, item.quantity);
   }
 
   const msg = `${itemInfo.itemName}을 획득했습니다.\n마을로 귀환하겠습니다!`;
