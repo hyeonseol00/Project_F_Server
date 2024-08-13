32da82fa596d04ccfe58601b66700e46051b562e
hyeonseol00 <hyeonseol22@naver.com>
2024-08-13 21:34:20 +0900

    🐛 bugfix : 게임 종료 시 leavetown 오류 픽스

--- src/handlers/town/leave.handler.js
+++ src/handlers/town/leave.handler.js
@@ -2,17 +2,20 @@ import { config } from '../../config/config.js';
 import { handleError } from '../../utils/error/errorHandler.js';
 import { getGameSession } from '../../session/game.session.js';
 import { createResponse } from '../../utils/response/createResponse.js';
+import { getUserByNickname } from '../../session/user.session.js';
 
-const leaveTownHandler = async (socket, curUser) => {
+const leaveTownHandler = async (socket, payload) => {
   const gameSession = getGameSession(config.session.townId);
 
   try {
     // 게임 세션에 저장된 모든 플레이어의 정보를 가져옴
     const playerIds = [];
-    playerIds.push(curUser.playerId);
+    playerIds.push(payload.playerId);
 
     // 각 유저에게 본인을 제외한 해당 플레이어 아이디 전송
-    for (const user of gameSession.users) {
+    for (const nickname of gameSession.playerNicknames) {
+      const user = getUserByNickname(nickname);
+
       // 해당 유저에게 다른 유저들을 스폰(해당 유저 제외)
       const despawnTownResponse = createResponse('response', 'S_Despawn', {
         playerIds,
