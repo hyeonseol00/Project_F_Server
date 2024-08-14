1eabca332737c4c0dbf8ae1caf7606c737a9f672
hyeonseol00 <hyeonseol22@naver.com>
2024-08-14 11:18:24 +0900

    🐛 bugfix : 함수 인자값 없을 경우 오류 예외처리

--- src/classes/DBgateway/playerinfo.gateway.js
+++ src/classes/DBgateway/playerinfo.gateway.js
@@ -21,6 +21,9 @@ export const getPlayerInfo = async (socket) => {
 };
 
 export const setPlayerInfo = async (socket, playerInfo) => {
+  if (!socket) {
+    return null;
+  }
   for (const field in playerInfo) {
     await redisCli.hSet(
       `${playerInfoKey}${socket.remotePort}`,
@@ -32,16 +35,25 @@ export const setPlayerInfo = async (socket, playerInfo) => {
 
 // gold
 export const getGold = async (socket) => {
+  if (!socket) {
+    return null;
+  }
   const userGold = await redisCli.hGet(`${playerInfoKey}${socket.remotePort}`, 'gold');
   return userGold;
 };
 
 export const setGold = async (socket, updatedGold) => {
+  if (!socket) {
+    return null;
+  }
   await redisCli.hSet(`${playerInfoKey}${socket.remotePort}`, 'gold', JSON.stringify(updatedGold));
 };
 
 // statinfo
 export const getStatInfo = async (socket) => {
+  if (!socket) {
+    return null;
+  }
   const statInfo = JSON.parse(
     await redisCli.hGet(`${playerInfoKey}${socket.remotePort}`, 'statInfo'),
   );
@@ -49,11 +61,17 @@ export const getStatInfo = async (socket) => {
 };
 
 export const setStatInfo = async (socket, statInfo) => {
+  if (!socket) {
+    return null;
+  }
   await redisCli.hSet(`${playerInfoKey}${socket.remotePort}`, 'statInfo', JSON.stringify(statInfo));
 };
 
 // inven
 export const getInven = async (socket) => {
+  if (!socket) {
+    return null;
+  }
   const { items } = JSON.parse(
     await redisCli.hGet(`${playerInfoKey}${socket.remotePort}`, 'inven'),
   );
@@ -61,6 +79,9 @@ export const getInven = async (socket) => {
 };
 
 export const setInven = async (socket, updatedInven) => {
+  if (!socket) {
+    return null;
+  }
   const inven = {};
   inven.items = [...updatedInven];
   await redisCli.hSet(`${playerInfoKey}${socket.remotePort}`, 'inven', JSON.stringify(inven));
@@ -68,6 +89,9 @@ export const setInven = async (socket, updatedInven) => {
 
 // equipment
 export const getEquipment = async (socket) => {
+  if (!socket) {
+    return null;
+  }
   const inventory = JSON.parse(
     await redisCli.hGet(`${playerInfoKey}${socket.remotePort}`, 'equipment'),
   );
@@ -75,6 +99,9 @@ export const getEquipment = async (socket) => {
 };
 
 export const setEquipment = async (socket, updatedEquipment) => {
+  if (!socket) {
+    return null;
+  }
   await redisCli.hSet(
     `${playerInfoKey}${socket.remotePort}`,
     'equipment',
@@ -84,17 +111,26 @@ export const setEquipment = async (socket, updatedEquipment) => {
 
 // level
 export const getLevel = async (socket) => {
+  if (!socket) {
+    return null;
+  }
   const statInfo = await getStatInfo(socket);
   return statInfo.level;
 };
 
 // How...?
 export const skillPointUpdate = (socket, skillPoint) => {
+  if (!socket) {
+    return null;
+  }
   const user = getUserBySocket(socket);
   user.skillPoint = skillPoint;
 };
 
 export const setWorldLevel = (socket, worldLevel) => {
+  if (!socket) {
+    return null;
+  }
   const user = getUserBySocket(socket);
   user.worldLevel = worldLevel;
 };
@@ -104,6 +140,9 @@ export const setWorldLevel = (socket, worldLevel) => {
 // ==============item=============
 
 export const getPotionsAccount = async (socket) => {
+  if (!socket) {
+    return null;
+  }
   const items = await getInven(socket);
   let count = 0;
   for (const item of items) {
@@ -114,6 +153,9 @@ export const getPotionsAccount = async (socket) => {
 };
 
 export const getItemIdx = async (socket, id) => {
+  if (!socket) {
+    return null;
+  }
   const inventory = await getInven(socket);
   for (const itemIdx in inventory) {
     if (inventory[itemIdx].id === id) {
@@ -124,6 +166,9 @@ export const getItemIdx = async (socket, id) => {
 };
 
 export const getItem = async (socket, id) => {
+  if (!socket) {
+    return null;
+  }
   const items = await getInven(socket);
   const findItem = items.find((item) => item.id === id);
 
@@ -131,12 +176,18 @@ export const getItem = async (socket, id) => {
 };
 
 export const pushItem = async (socket, item) => {
+  if (!socket) {
+    return null;
+  }
   const items = await getInven(socket);
   items.push(item);
   await setInven(socket, items);
 };
 
 export const deleteItem = async (socket, id) => {
+  if (!socket) {
+    return null;
+  }
   const items = await getInven(socket);
   const findIdx = items.findIndex((item) => item.id === id);
   if (findIdx !== -1) {
@@ -146,6 +197,9 @@ export const deleteItem = async (socket, id) => {
 };
 
 export const decItem = async (socket, id, quantity) => {
+  if (!socket) {
+    return null;
+  }
   const items = await getInven(socket);
   const findIdx = items.findIndex((item) => item.id === id);
   items[findIdx].quantity -= quantity;
@@ -153,6 +207,9 @@ export const decItem = async (socket, id, quantity) => {
 };
 
 export const addItem = async (socket, id, quantity) => {
+  if (!socket) {
+    return null;
+  }
   const items = await getInven(socket);
   const findIdx = items.findIndex((item) => item.id === id);
   items[findIdx].quantity += quantity;
@@ -160,12 +217,18 @@ export const addItem = async (socket, id, quantity) => {
 };
 
 export const setItemId = async (socket, itemType, id) => {
+  if (!socket) {
+    return null;
+  }
   const equipment = await getEquipment(socket);
   equipment[itemType] = id;
   await setEquipment(socket, equipment);
 };
 
 export const getItemQuantity = async (socket, id) => {
+  if (!socket) {
+    return null;
+  }
   const items = await getInven(socket);
   const findItem = items.find((item) => item.id === id);
   if (findItem) {
@@ -175,6 +238,9 @@ export const getItemQuantity = async (socket, id) => {
 };
 
 export const getPotionItems = async (socket) => {
+  if (!socket) {
+    return null;
+  }
   const items = await getInven(socket);
   const potions = [];
   for (const item of items) {
@@ -189,6 +255,9 @@ export const getPotionItems = async (socket) => {
 
 // ==========team==========
 export const getTeam = async (socket) => {
+  if (!socket) {
+    return { teamId: null, isOwner: null };
+  }
   const teamId = await redisCli.hGet(`${playerInfoKey}${socket.remotePort}`, 'teamId');
   const isOwner = await redisCli.hGet(`${playerInfoKey}${socket.remotePort}`, 'isOwner');
 
@@ -196,16 +265,25 @@ export const getTeam = async (socket) => {
 };
 
 export const setTeam = async (socket, teamId, isOwner = null) => {
+  if (!socket) {
+    return null;
+  }
   await redisCli.hSet(`${playerInfoKey}${socket.remotePort}`, 'teamId', teamId);
   await redisCli.hSet(`${playerInfoKey}${socket.remotePort}`, 'isOwner', isOwner);
 };
 
 export const getInvitedTeams = async (socket) => {
+  if (!socket) {
+    return null;
+  }
   const invitedTeams = await redisCli.hGet(`${playerInfoKey}${socket.remotePort}`, 'invitedTeams');
   return invitedTeams;
 };
 
 export const setInvitedTeams = async (socket, updatedInvitedTeams) => {
+  if (!socket) {
+    return null;
+  }
   await redisCli.hSet(
     `${playerInfoKey}${socket.remotePort}`,
     'invitedTeams',
