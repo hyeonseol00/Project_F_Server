d0864f39d66b67df1e79b2e4619c885a95d12e81
hyeonseol00 <hyeonseol22@naver.com>
2024-08-19 19:22:24 +0900

    🩹 fix : 보스 공격 인식 범위 조정

--- src/constants/hatchery/sceneConfig.js
+++ src/constants/hatchery/sceneConfig.js
@@ -3,6 +3,6 @@ export const BOSS_ID = 2028;
 export const BOSS_INIT_TRANSFORM = { posX: -3.5, posY: -19.5, posZ: -315.0, rot: 0 };
 export const BOSS_TARGET_INTERVAL_ID = 'bossTargetPlayer';
 export const BOSS_TARGET_INTERVAL = 500;
-export const BOSS_ATTACK_RANGE = 2;
+export const BOSS_ATTACK_RANGE = 4;
 export const DROP_ITEMS_QUANTITY = 6;
 export const BOSS_ATTACK_SPEED = 3000;
